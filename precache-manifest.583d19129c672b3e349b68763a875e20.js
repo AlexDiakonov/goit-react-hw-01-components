c898793a096067723d1d234b16a9c5cd61493780
AlexDiakonov <imalex.diakonov@gmail.com>
2020-08-03 14:57:31 +0300

    Updates

---
+++ precache-manifest.583d19129c672b3e349b68763a875e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842925a7b28149fb5b34433d8993c8ec",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a97025e48039a98caba9",
+    "url": "/goit-react-hw-01-components/static/css/main.6a6c3c09.chunk.css"
+  },
+  {
+    "revision": "152533194c03035efb60",
+    "url": "/goit-react-hw-01-components/static/js/2.4b29e457.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.4b29e457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97025e48039a98caba9",
+    "url": "/goit-react-hw-01-components/static/js/main.47f54b2e.chunk.js"
+  },
+  {
+    "revision": "9658c93aaae188b60c8c",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.649194bf.js"
+  }
+]);
